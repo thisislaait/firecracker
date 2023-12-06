95de70ee4f98a3e157aa10679377a8021dd66ac4
thisislaait <ogadi.ugo@gmail.com>
2023-12-06 11:20:17 +0100

    Fix: window.h error no such file or dir

--- src/execute.c
+++ src/execute.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h> /* Include only for Windows */
+/*#include <windows.h> Include only for Windows */
 #include "shell.h"
 
 void executeCommand(char* command) {
