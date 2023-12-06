9de81ae0f0e15899d886cf455708d548ccdd2004
thisislaait <ogadi.ugo@gmail.com>
2023-12-06 13:48:37 +0100

    Add: tokenization

--- src/execute.c
+++ src/execute.c
@@ -18,66 +18,65 @@
 
 void executeCommand(char *command)
 {
-	/* Split the command into executable and arguments */
-	char *executable = strtok(command, " ");
-	char *arguments[10];
-	pid_t pid = fork();
+    /* Split the command into executable and arguments */
+    char *token;
+    char *argv[20];  /*Adjust the array size as needed*/
+    pid_t pid;
 
-	int i = 0;
-	int j;
+    int i = 0;
+    int j;
 
-	if (command == NULL)
-	{
-		fprintf(stderr, "Error: Unexpected end of input\n");
-		exit(EXIT_FAILURE);
-	}
+    if (command == NULL)
+    {
+        fprintf(stderr, "Error: Unexpected end of input\n");
+        exit(EXIT_FAILURE);
+    }
 
-	while (i < 9 && (arguments[i] = strtok(NULL, " ")) != NULL)
-	{
-		i++;
-	}
-	arguments[i] = NULL; /* Null-terminate the argument list */
+    /* Tokenize the command */
+    token = strtok(command, " ");
+    while (token != NULL && i < 19)  /*Adjust the limit as needed*/
+    {
+        argv[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    argv[i] = NULL; /* Null-terminate the argument list */
 
-	if (pid == -1)
-	{
-		perror("Error forking process");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		/* Child process */
-		/* Debugging prints */
-		printf("Executable: %s\n", executable);
+    pid = fork();
 
-        for (j = 0; arguments[j] != NULL; ++j) {
-            printf("Argument %d: %s\n", j, arguments[j]);
+    if (pid == -1)
+    {
+        perror("Error forking process");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        /* Child process */
+        execvp(argv[0], argv);
+
+        /* If execvp fails */
+        perror("Error executing command");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        /* Parent process */
+        int status;
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status))
+        {
+            int exitCode = WEXITSTATUS(status);
+            if (exitCode != 0)
+            {
+                fprintf(stderr, "Command exited with code %d\n", exitCode);
+            }
         }
+        else
+        {
+            fprintf(stderr, "Command did not exit successfully\n");
+        }
+    }
+}
 
-		execvp(executable, arguments);
-
-		/* If execvp fails */
-		perror("Error executing command");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		/* Parent process */
-		int status;
-		waitpid(pid, &status, 0);
-
-		if (WIFEXITED(status))
-		{
-			int exitCode = WEXITSTATUS(status);
-			if (exitCode != 0)
-
-			{
-				fprintf(stderr, "Command exited with code %d\n", exitCode);
-			}
-		}
-		else
-		{
-			fprintf(stderr, "Command did not exit successfully\n");
-		}
-	}
 }
 
