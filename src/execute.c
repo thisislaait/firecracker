c6d41c87bdf47bfa016cd3b5e750b948424e44e9
thisislaait <ogadi.ugo@gmail.com>
2023-12-07 01:22:15 +0100

    back to argument function

--- src/execute.c
+++ src/execute.c
@@ -14,119 +14,92 @@
  */
 static void splitCommand(char *command, char *argv[], int maxArgs)
 {
-    char *token;
-    int i = 0;
+	char *token;
+	int i = 0;
 
-    if (command == NULL)
-    {
-        fprintf(stderr, "Error: Unexpected end of input\n");
-        exit(EXIT_FAILURE);
-    }
-    token = strtok(command, " ");
-    while (token != NULL && i < maxArgs - 1)
-    {
-        argv[i++] = token;
-        token = strtok(NULL, " ");
-    }
-    argv[i] = NULL; /* Null-terminate the argument list */
+	if (command == NULL)
+	{
+		fprintf(stderr, "Error: Unexpected end of input\n");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(command, " ");
+	while (token != NULL && i < maxArgs - 1)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL; /* Null-terminate the argument list */
 }
 
 /**
  * executeChild - Executes the command in the child process.
+ * @full_path: The full path to the executable.
  * @argv: The array of arguments.
  */
 static void executeChild(char *argv[])
 {
-    execvp(argv[0], argv);
-    /* If execvp fails */
-    perror("Error executing command");
-    exit(EXIT_FAILURE);
+	execvp(argv[0], argv);
+	/* If execvp fails */
+	perror("Error executing command");
+	exit(EXIT_FAILURE);
 }
 
 /**
  * executeParent - Waits for the child process to complete.
  * @pid: The process ID of the child.
- 
+ */
 static void executeParent(pid_t pid)
 {
-    int status;
+	int status;
 
-    waitpid(pid, &status, 0);
-    if (WIFEXITED(status))
-    {
-        int exitCode = WEXITSTATUS(status);
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+	{
+		int exitCode = WEXITSTATUS(status);
 
-        if (exitCode != 0)
-        {
-            fprintf(stderr, "Command exited with code %d\n", exitCode);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "Command did not exit successfully\n");
-    }
-}*/
-
-/**
- * searchAndExecute - Searches for the executable in the PATH.
- * @command: The command to execute.
- */
-static void searchAndExecute(char *command)
-{
-    char *path = getenv("PATH");
-    char *token = strtok(path, ":");
-    char *fullPath;
-    char *argv[20];
-
-    while (token != NULL)
-    {
-        fullPath = malloc(strlen(token) + strlen(command) + 2);
-        if (fullPath == NULL)
-        {
-            perror("Malloc failed");
-            exit(EXIT_FAILURE);
-        }
-
-        strcpy(fullPath, token);
-        strcat(fullPath, "/");
-        strcat(fullPath, command);
-
-        if (access(fullPath, X_OK) == 0)
-        {
-            splitCommand(fullPath, argv, 20);
-            executeChild(argv);
-            free(fullPath);
-            return;
-        }
-
-        free(fullPath);
-        token = strtok(NULL, ":");
-    }
-
-    fprintf(stderr, "Command not found: %s\n", command);
-    exit(EXIT_FAILURE);
+		if (exitCode != 0)
+		{
+			fprintf(stderr, "Command exited with code %d\n", exitCode);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Command did not exit successfully\n");
+	}
 }
+
+
 
 /**
  * executeCommand - Executes a shell command.
  * @command: The command to execute.
- * Description: Executes a shell command by checking if the command exists in PATH,
- * and handling the results of the command execution.
+ * Description: Executes a shell command by forking a new process,
+ * replacing its image with the specified command, and
+ * handling the results of the command execution.
  *
  * Return: This function does not return a value.
  */
 void executeCommand(char *command)
 {
-    /*Check if the command exists in the PATH*/
-    if (access(command, X_OK) == 0)
-    {
-		char *argv[2];
-		argv[0] = command;
-		argv[1] = NULL;
-        executeChild(argv);
-        return;
-    }
+	char *argv[20]; /* Adjust the array size as needed */
+	pid_t pid;
 
-    /* If not found in current directory, search in PATH*/
-    searchAndExecute(command);
+	splitCommand(command, argv, 20);
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error forking process");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		/* Child process */
+		executeChild(argv);
+	}
+	else
+	{
+		/* Parent process */
+		executeParent(pid);
+	}
 }
